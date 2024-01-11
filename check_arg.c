af4eb1a89bcb22636a9099ec7a70f35f92b610fe
Zigouingouin <61563425+ft-Ali@users.noreply.github.com>
2024-01-11 16:11:22 +0100

    Update check_arg.c

--- check_arg.c
+++ check_arg.c
@@ -14,29 +14,52 @@
 
 void	check_error(void)
 {
-	write(1, "Error\n", 6);
+	write(2, "Error\n", 6);
+	exit(EXIT_FAILURE);
 }
-
-void	check_arg(int argc, **argv)
+int	check_double(int argc, char **argv)
 {
 	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
+				check_error();
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+void	check_arg(int argc, char **argv)
+
+{
+	int i;
+	int len;
+
+	i = 1;
 	if (argc >= 2)
 	{
-		i = 0;
-		while (argv[i])
+		while (i < argc)
 		{
-			if (!ft_atoi(argv[i]) && !check_double())
+			len = 0;
+			while (argv[i][len] != '\0')
+			{
+				if (!ft_isdigit(argv[i][len]) && argv[i][len] != '-'
+					&& argv[i][len] != '+')
+				{
+					check_error();
+				}
+				len++;
+			}
 			i++;
 		}
-	}
-}
-
-int	check_double(int argc, **argv)
-{
-	int i = 1;
-
-	while(i < argc )
-	{
-		
+		if (check_double(argc, argv))
+			check_error();
 	}
 }
